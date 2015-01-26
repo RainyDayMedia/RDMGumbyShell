50fb8f3781bd49689a942456d7065193c7bf5131
Todd Miller <toddses@gmail.com>
2015-01-26 13:00:29 -0500

    adds and updates templates

--- content-page.php
+++ content-page.php
@@ -2,7 +2,7 @@
 /**
  * The template used for displaying page content in page.php
  *
- * @package gumby-wordpress-theme
+ * @package rdmgumby
  */
 ?>
 
@@ -13,6 +13,12 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'rdmgumby' ),
+				'after'  => '</div>',
+			) );
+		?>
 	</div><!-- .entry-content -->
-	
+	<?php edit_post_link( __( 'Edit', 'rdmgumby' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
