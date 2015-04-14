2ed994279b59b6d2e9675c6e27cebd6aa932f71b
Todd Miller <toddses@gmail.com>
2015-04-14 13:42:43 -0400

    major refactor of the included templates

--- index.php
+++ index.php
@@ -12,9 +12,6 @@
  */
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
@@ -36,8 +33,4 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
